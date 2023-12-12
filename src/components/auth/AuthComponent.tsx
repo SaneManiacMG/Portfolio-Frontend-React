28a56b54531e909ce2a3fa20b0e5302da012cb6a
SaneManiacMG <mmoroane@hotmail.com>
2023-12-12 13:36:11 +0200

    Fixed box alignment

--- src/components/auth/AuthComponent.tsx
+++ src/components/auth/AuthComponent.tsx
@@ -1,18 +1,37 @@
 import '../../styles/AuthStyles.css';
 
+type onClick = () => void;
+
 const AuthComponent = () => {
-    
+    const loginClicked: onClick = () => {
+        console.log('login clicked');
+        alert('login clicked');
+    }
+
+    const registerClicked: onClick = () => {
+        console.log('register clicked');
+        alert('register clicked');
+    }
 
     return (
         <div className='container'>
-            <ul className='tabs'>
-                <li className='leftTab '>
-                    <a href='#login'>Login</a>
-                </li>
-                <li className='rightTab'>
-                    <a href='#register'>Register</a>
-                </li>
-            </ul>
+            <div>
+                <ul className='tabs'>
+                    <li className='leftTab '>
+                        <a
+                            onClick={loginClicked}
+                            href='#login'>Login</a>
+                    </li>
+                    <li className='rightTab'>
+                        <a
+                            onClick={registerClicked}
+                            href='#register'>Register</a>
+                    </li>
+                </ul>
+            </div>
+            <div className='tabContent'>
+                Some dummy content
+            </div>
         </div>
     );
 };
